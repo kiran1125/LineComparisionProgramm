ba8faa4de24623994d9dd5947cdb7ca24256eb97
kiran1125 <90588261+kiran1125@users.noreply.github.com>
2021-10-14 08:04:04 +0530

    Started Line Comparision Computation Program

---
+++ src/com/bridgeLabz/LineComparision.java
@@ -0,0 +1,7 @@
+package com.bridgeLabz;
+
+public class LineComparision {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Line Comparision Computation program");
+    }
+}
