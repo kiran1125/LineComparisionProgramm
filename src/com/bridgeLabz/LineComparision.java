d16b1cb00416c7bb3bcd3d2cfaf75cd17de43d48
kiran1125 <90588261+kiran1125@users.noreply.github.com>
2021-10-14 08:16:03 +0530

    Added Calculation of Line

--- src/com/bridgeLabz/LineComparision.java
+++ src/com/bridgeLabz/LineComparision.java
@@ -1,7 +1,12 @@
 package com.bridgeLabz;
 
+import java.lang.Math;
 public class LineComparision {
     public static void main(String[] args) {
         System.out.println("Welcome to the Line Comparision Computation program");
+        double x1=5,x2=6,y1=4,y2=5;
+        double length;
+        length = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
+        System.out.println("length of the line : " +length);
     }
 }
